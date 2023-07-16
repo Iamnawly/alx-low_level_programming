e9713b3d2cf6e2cfd946d7704114e1bc9a307621
iamnawly <apetefejiro1@gmail.com>
2023-07-16 05:17:28 -0700

    Hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 /**
  * main - Entry point
- *
+ * written by apete
  * Return: Always 0 (Success)
  */
 int main(void)
 {
+	/*puts*/
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
