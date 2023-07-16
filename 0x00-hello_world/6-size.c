9d6a73fd381a12cb16d5f39767c4feaf8780242c
iamnawly <apetefejiro1@gmail.com>
2023-07-16 05:30:52 -0700

    Hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,12 @@
 #include<stdio.h>
 /**
 * main - Entry point
-*
+* written by kelvin
 * Return: Always 0 (Success)
 */
 int main(void)
-{
+{ 
+	/*printf*/
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
@@ -13,8 +14,3 @@ int main(void)
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
-
-100-intel
-
-#!/bin/bash
-gcc -S -masm=intel $CFILE
