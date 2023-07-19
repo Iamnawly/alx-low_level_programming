4c4273d9d803d2b7a8ab620ad6c1fea4a2c6415f
iamnawly <apetefejiro1@gmail.com>
2023-07-18 19:06:58 -0700

    Functions and nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @a: int to be added to b
+ * @b: int to be added to a
+ * written by apete
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	/*int*/
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * written by apete
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+int _isalpha(int c)
+{
+	/*int*/
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
