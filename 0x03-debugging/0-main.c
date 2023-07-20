10bccd88d5b0280b8ea7add77a6454eda925cc67
iamnawly <apetefejiro1@gmail.com>
2023-07-20 16:56:01 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* main - Test function for positive or negative
+* Return: 0
+* written by apete
+*/
+int main(void)
+{
+/*int*/
+int i;
+i = 0;
+positive_or_negative(i);
+return (0);
+}
