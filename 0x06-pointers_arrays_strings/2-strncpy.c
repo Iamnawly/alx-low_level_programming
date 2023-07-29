0ccac27c2600073eafdb8beaf042d8ce776e4f3e
iamnawly <apetefejiro1@gmail.com>
2023-07-28 17:40:43 -0700

    Pointers and function

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * written by apete
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	/*printf*/
+	int j;
+
+	j = 0;
+	while (j < n && src[j] != '\0')
+	{
+		dest[j] = src[j];
+		j++;
+	}
+	while (j < n)
+	{
+		dest[j] = '\0';
+		j++;
+	}
+
+	return (dest);
+}
