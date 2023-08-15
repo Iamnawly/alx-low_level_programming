d55b13f6056d2d026a40ac76f28d3e1c405de7e4
Iamnawly <apetefejiro1@gmail.com>
2023-08-15 14:28:01 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - prints the name of the file
+ * written by apete
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	/*printf*/
+	printf("%s\n", __FILE__);
+	return (0);
+}
