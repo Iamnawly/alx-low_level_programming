6dbf3e8eda0a2fc250bb6103e412f816e96595f6
iamnawly <apetefejiro1@gmail.com>
2023-08-29 16:02:30 -0700

    Singly linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * free_listint2 - frees a linked list
+ * free_listint - frees a linked list
  * written by apete
  * @head: pointer to the listint_t list to be freed
  */
